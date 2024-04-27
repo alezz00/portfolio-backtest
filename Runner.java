cd7ba4d606915b990723ae86e7803d8ec6fd867f
Alessio <165487464+alezz00@users.noreply.github.com>
2024-04-27 22:05:03 +0200

    Refactor

--- Runner.java
+++ Runner.java
@@ -1,13 +1,16 @@
 package runner;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -15,20 +18,44 @@ public class Runner {
 
 	public static void main(String[] args) throws Exception {
 
-		final Scanner scanner = new Scanner(System.in);
+		// Find all csv
+		final File[] csvs = new File(System.getProperty("user.dir") + "/portfolios").listFiles(f -> f.isFile() && f.getName().toLowerCase().endsWith(".csv"));
+		if (csvs.length == 0) {
+			System.out.println("Your 'portfolios' subfolder is empty!");
+			System.exit(0);
+		}
 
-		System.out.println("Csv file to read:");
-		final String file = scanner.nextLine();
+		// Ask the info
+		final Scanner scanner = new Scanner(System.in);
+		System.out.println("Choose the portfolio:");
+
+		final Map<Integer, File> csvMap = new HashMap<>();
+		int csvIndex = 1;
+		for (final File csv : csvs) {
+			final int index = csvIndex++;
+			csvMap.put(index, csv);
+			System.out.println(" -%s %s".formatted(index, csv.getName()));
+		}
+
+		int fileIndex = scanner.nextInt();
+
+		while (!csvMap.containsKey(fileIndex)) {
+			System.out.println("Number not valid... Try again");
+			fileIndex = scanner.nextInt();
+		}
+
+		final File file = csvMap.get(fileIndex);
+		System.out.println("Chosen: " + file.getName());
 
 		System.out.println("Year gap:");
 		final int years = scanner.nextInt();
 
 		scanner.close();
 
-		final List<IndexSnapshot> records = new ArrayList<>();
+		final List<FileRow> rows = new ArrayList<>();
 
 		// Read the file
-		try (BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/portfolios/" + file + ".csv"))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 			boolean firstLine = true;
 			String line;
 			while ((line = br.readLine()) != null) {
@@ -37,27 +64,32 @@ public class Runner {
 				final String stringedDate = values[0];
 				final int year = Integer.valueOf(stringedDate.substring(0, 4));
 				final int month = Integer.valueOf(stringedDate.substring(5));
-				final LocalDate date = LocalDate.of(year, month, 1); // let's pretend it's the first day of the month
+				final LocalDate date = LocalDate.of(year, month, 1);
 				final int price = BigDecimal.valueOf(Double.valueOf(values[1])).intValue();
 
-				final IndexSnapshot snapshot = new IndexSnapshot(date, price);
-				records.add(snapshot);
+				rows.add(new FileRow(date, price));
 			}
 		}
 
 		// Calculate the percentage difference between the dates
 		final List<Integer> percentageDifferences = new ArrayList<>();
 
-		for (final IndexSnapshot snapshot : records) {
+		for (final FileRow row : rows) {
 
 			// search the end date (gap=6 ---> 2004->2010)
-			final IndexSnapshot next = records.stream().filter(rec -> Objects.equals(rec.date, snapshot.date.plusYears(years))).findFirst().orElse(null);
-			if (next == null) { break; }
+			FileRow next = rows.stream().filter(rec -> Objects.equals(rec.date, row.date.plusYears(years))).findFirst().orElse(null);
+			if (next == null) {
+				if (percentageDifferences.isEmpty()) {
+					next = rows.get(rows.size() - 1);
+				} else {
+					break;
+				}
+			}
 
-			final int percentageDifference = (next.price * 100 / snapshot.price) - 100;
+			final int percentageDifference = (next.price * 100 / row.price) - 100;
 
 			final String sign = percentageDifference > 0 ? "+" : "";
-			System.out.println(snapshot.date + " -> " + next.date + " -> " + sign + percentageDifference + "%");
+			System.out.println(row.stringDate() + " -> " + next.stringDate() + " ---> " + sign + percentageDifference + "%");
 			percentageDifferences.add(percentageDifference);
 		}
 
@@ -81,6 +113,7 @@ public class Runner {
 
 	/** Calculates the median of the numbers in the list. */
 	private static int median(List<Integer> list) {
+		if (list.size() == 1) { return list.get(0); }
 		list.sort(Comparator.naturalOrder());
 
 		final List<Integer> positions = new ArrayList<>();
@@ -117,7 +150,12 @@ public class Runner {
 		return (int) Math.sqrt(standardDeviation / list.size());
 	}
 
-	private static record IndexSnapshot(LocalDate date, int price) {
+	private static record FileRow(LocalDate date, int price) {
+		public String stringDate() {
+			String month = String.valueOf(date.getMonthValue());
+			if (month.length() == 1) { month = "0" + month; }
+			return date.getYear() + "-" + month;
+		}
 	}
 
 }
